d53738149f1fed2e379a3166f43ea14a200f2101
Kick Buttowski <kb269099@gmail.com>
2024-09-21 13:10:04 +0530

    chore: room cards 3 columns

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -39,7 +39,7 @@ export function HomePage() {
 
       {!isLoading && Boolean(rooms?.length) && (
         <>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 pb-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 pb-8">
             {rooms?.map(room => {
               return <RoomCard key={room.id} room={room} />
             })}
